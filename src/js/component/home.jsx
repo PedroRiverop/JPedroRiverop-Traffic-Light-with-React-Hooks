e4dd7db3616b3ac7478f2d416b35c5226a62ed53
Pedro <jprivero4@gmail.com>
2024-09-27 13:19:08 -0400

    Se incluyo el boton para añadir color

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,29 +6,41 @@ import rigoImage from "../../img/rigo-baby.jpg";
 // boxShadow: '0px 0px 9px 12px red'
 
 const styleLights = {width : 110, height : 110};
-const lights = ["red", "yellow", "green"]
+
 
 
 	
 //create your first component
 const Home = () => {
+	const [styleBox, setStyleBox] = useState({width : 200, height : 400}) 
+	const [lights, setLights] = useState(["red", "yellow", "green"]);
+
 	const [ shadow, setShadow] = useState("");
-	//const [selected, setSelected] = useState("")
-	//const styleShadow ={ boxShadow: '0px 0px 9px 12px ' + ((color===shadow)? shadow : "")} 
 
 	function handlerClick (index){
 		setShadow(lights[index])
 		}
+
+	function handlerAddColor() {
+		if (lights.length === 3){
+		setLights(["red", "yellow", "green", "purple"]);
+		setStyleBox({width : 200, height : 550});
+		} else {setLights(["red", "yellow", "green"]); setStyleBox({width : 200, height : 400})}
+	}	
 	return (
 		<div className="d-flex flex-column align-items-center">
 			<div className="bg-black" style={{width : 20, height : 150}}></div>
-			<div className="bg-black d-flex flex-column align-items-center" style={{width : 200, height : 400}} >
+			<div className="bg-black d-flex flex-column align-items-center" style={styleBox} >
 				{lights.map((color, index)=>(
 				<div key={index} 
 					className="rounded-circle my-auto" 
 					style={{ ...styleLights, boxShadow: '0px 0px 9px 12px ' + ((color===shadow)? shadow : ""), background: color}}
-					onClick={()=>handlerClick(index)} ></div>))}
+					onClick={()=>handlerClick(index)} >
+
+				</div>))}
+				
 			</div>
+			<button onClick={handlerAddColor}>Purple Light</button>
 		</div>
 	);
 };
